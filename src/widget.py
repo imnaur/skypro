b3a8d411769de1e1ea6c65556ac9a076fd5c0d65
Nargiza Imasheva <imnaur@bk.ru>
2025-09-16 10:27:12 +0200

    Новый виджет ДЗ

---
+++ src/widget.py
@@ -0,0 +1,22 @@
+def mask_account_card(card_number: str) -> str:
+    if "Счет" in card_number:
+        return "Счет " + "**" + card_number[-4:]
+    elif "Maestro" or "Mastercard" or "Visa" in card_number:
+        masked_num = card_number[-16:]
+        start_num = masked_num[:6]
+        stars_num = "*" * 6
+        end_num = masked_num[-4:]
+        finish_num = start_num + stars_num + end_num
+        card_number_result = " ".join(finish_num[i : i + 4] for i in range(0, len(masked_num), 4))
+    return card_number[:-16] + card_number_result
+
+
+
+
+def get_date(date_info: str) -> str:
+    changed_date = date_info[:10]
+    correct_date = changed_date.replace("-", "")
+    day_date = correct_date[-2:]
+    month_date = correct_date[-4:-2]
+    year_date = correct_date[:4]
+    return f"{day_date}.{month_date}.{year_date}"
