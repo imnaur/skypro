073ef6fc70e273514aade69b9ae108ba2d357d13
Nargiza Imasheva <imnaur@bk.ru>
2025-09-20 12:45:12 +0200

    Доработанное ДЗ

--- src/processing.py
+++ src/processing.py
@@ -1,14 +1,13 @@
-def filter_by_state(list_of_state: list[dict], state="EXECUTED") -> list[dict]:
-    """Функция, которая фильтрует словари по состоянию."""
-    new_state_list = []
-    for state in list_of_state:
-        for key, value in state.items():
-            if value == "EXECUTED":
-                new_state_list.append(state)
-    return new_state_list
+def filter_by_state(list_of_states: list[dict], state: str = "EXECUTED") -> list[dict]:
+    """Функция, которая фильтрует список словарей по значению ключа 'state'."""
+    new_states_list = []
+    for item in list_of_states:
+        if item.get("state") == state:
+            new_states_list.append(item)
+    return new_states_list
 
 
-def sort_by_date(data: list[dict]) -> list[dict]:
-    """Функиця, которая сортирует список по порядку убывания."""
-    sorted_date = sorted(data, key=lambda x: x["date"], reverse=True)
+def sort_by_date(data: list[dict], reverse: bool = True) -> list[dict]:
+    """Функиця, которая сортирует список словарей по ключу 'date'."""
+    sorted_date = sorted(data, key=lambda x: x["date"], reverse=reverse)
     return sorted_date
